c60236000db4dc9fac3691f83916fb90747a1e06
Sol <sodanabe@gmail.com>
2023-05-07 18:11:19 +0300

    Initial commit

---
+++ src/main/java/hac/longpolling/BakeryController.java
@@ -0,0 +1,2 @@
+package hac.longpolling;public class BakeryController {
+}
---
+++ src/test/java/hac/longpolling/LongPollingApplicationTests.java
@@ -0,0 +1,13 @@
+package hac.longpolling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongPollingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
